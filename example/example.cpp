cfbeb83b90548429c13d7655cff2f6a813299b5f
Vic P <vic4key@gmail.com>
2021-05-11 01:33:32 +0700

    Initial

--- example/example.cpp
+++ example/example.cpp
@@ -6,11 +6,34 @@
 #include "example.events.h"
 #include "example.shader.h"
 
+class GLWindowExample : public GLWindow
+{
+public:
+  GLWindowExample() : GLWindow() {};
+  virtual ~GLWindowExample() {};
+
+  virtual void initial()
+  {
+    // DO NOTHING
+  }
+
+  virtual void final()
+  {
+    // DO NOTHING
+  }
+
+  virtual void on_display()
+  {
+    // DO NOTHING
+  }
+};
+
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
+  GLWindowExample gl;
   // GLWindowExampleSimple gl;
   // GLWindowExampleEvents gl;
-  GLWindowExampleShader gl;
+  // GLWindowExampleShader gl;
   gl.run();
 
   return EXIT_SUCCESS;
