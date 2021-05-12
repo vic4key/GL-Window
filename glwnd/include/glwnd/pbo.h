0e4853bbd9486ec1bbb81753437393c193f192e2
Vic P <vic4key@gmail.com>
2021-05-12 18:43:17 +0700

    Initial

---
+++ glwnd/include/glwnd/pbo.h
@@ -0,0 +1,38 @@
+#pragma once
+
+/**
+ * @file   pbo.h
+ * @author Vic P.
+ * @brief  Pixel Buffer Object (PBO)
+ */
+
+#include <GL/glew.h>
+
+#include <functional>
+
+class PBO
+{
+public:
+  PBO();
+  virtual ~PBO();
+
+  bool setup(GLvoid* ptr_pixel_data, int width, int height, int channel, GLint image_format, GLint display_format);
+
+  bool TexImage2D();
+
+private:
+  bool unpack();
+  bool pack(std::function<void(const void* ptr)> fn);
+
+private:
+  GLuint m_id = 0;
+
+  GLint m_display_format;
+  GLvoid* m_ptr_pixel_data;
+  int m_width;
+  int m_height;
+  int m_channel;
+  GLint m_format;
+
+  bool m_ready;
+};
