feca14a6b941428e0b24c77038644e2e1210d65c
Vic P <vic4key@gmail.com>
2021-06-20 04:47:12 +0700

    Initial

--- glwnd/src/view.cpp
+++ glwnd/src/view.cpp
@@ -52,6 +52,25 @@ glm::mat4 GLView::get_context_matrix(GLenum type)
   return matrix;
 }
 
+void GLView::set_context_matrix(GLenum type, const glm::mat4& matrix)
+{
+  GLint prev_mtx_mode = 0;
+  glGetIntegerv(GL_MATRIX_MODE, &prev_mtx_mode);
+  {
+    glMatrixMode(type);
+    glLoadMatrixf(glm::value_ptr(matrix));
+  }
+  glMatrixMode(prev_mtx_mode);
+}
+
+glm::mat4 GLView::transform_context_matrix(GLenum type, const glm::vec3& t, const glm::vec3& r, const glm::vec3& s)
+{
+  auto mtx = this->get_context_matrix(type);
+  mtx = this->transform_matrix(t, r, s, &mtx);
+  this->set_context_matrix(type, mtx);
+  return mtx;
+}
+
 glm::mat4 GLView::transform_matrix(const glm::vec3& t, const glm::vec3& r, const glm::vec3& s, glm::mat4* m)
 {
   glm::mat4 mtx = glm::mat4(1.F);
@@ -64,7 +83,7 @@ glm::mat4 GLView::transform_matrix(const glm::vec3& t, const glm::vec3& r, const
 
   if (m != nullptr)
   {
-    mtx *= *m;
+    mtx = *m * mtx;
   }
 
   return mtx;
