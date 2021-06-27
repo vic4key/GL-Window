09b975f0a9675c2f97a74af099daa7836e87442c
Vic P <vic4key@gmail.com>
2021-06-28 03:05:59 +0700

    Initial

--- example/example.text.h
+++ example/example.text.h
@@ -17,7 +17,7 @@ public:
   virtual void initial()
   {
     this->enable_coordiates();
-    // m_text_render_2d.initialize(this);
+    m_text_render_2d.initialize(this);
     m_font_id_Georgia = m_text_render_2d.add_font("C:\\Windows\\Fonts\\courbd.ttf", 24);
     m_font_id_Trebuchet_MS = m_text_render_2d.add_font("C:\\Windows\\Fonts\\trebucbd.ttf", 32);
   }
@@ -25,16 +25,16 @@ public:
   virtual void on_display()
   {
     //glColor3f(1.F, 1.F, 1.F);
-    //m_text_render_2d.render_text("this is a text renderer\nsupported multi-lines\nsupported multi-fonts\n...", p2d(-0.75, +0.75));
+    m_text_render_2d.render_text("this is a text renderer\nsupported multi-lines\nsupported multi-fonts\n...", p2d(-0.75, +0.75));
 
     glColor3f(0.F, 1.F, 0.F);
     this->text().render_text("Courier New", p2d(0., 0.));
 
     //glColor3f(1.F, 0.F, 0.F);
-    //m_text_render_2d.render_text("Georgia", p2i(50, 50), &m_font_id_Georgia);
+    m_text_render_2d.render_text("Georgia", p2i(50, 50), &m_font_id_Georgia);
 
     //glColor3f(0.F, 0.F, 1.F);
-    //m_text_render_2d.render_text("Trebuchet MS", p2i(50, 100), &m_font_id_Trebuchet_MS);
+    m_text_render_2d.render_text("Trebuchet MS", p2i(50, 100), &m_font_id_Trebuchet_MS);
   }
 
 private:
