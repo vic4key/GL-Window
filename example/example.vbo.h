cc14c8e4d46690cb93e03508164d5c564192b6dc
Vic P <vic4key@gmail.com>
2023-02-19 11:22:17 +0700

    Initial

--- example/example.vbo.h
+++ example/example.vbo.h
@@ -12,7 +12,7 @@ public:
 
   virtual void on_display()
   {
-    // With VBO
+    // With VBO - Eg. Format: |P|P|...|C|C|
 
     GLfloat positions[] =
     {
@@ -32,7 +32,7 @@ public:
       +0.5F, -1.0F,
       +1.0F, -1.0F,
     };
-    
+
     GLfloat colors[] =
     {
       1.0F, 0.0F, 0.0F,
@@ -51,43 +51,97 @@ public:
       1.0F, 0.0F, 1.0F,
       1.0F, 0.0F, 1.0F,
     };
-    
-    VBO vbo;
-    GLuint vds_size = 5 * sizeof(GLfloat); // this vds has 2 attributes (position 2f + color 3f = 5f)
-    vbo.initialize({{positions, sizeof(positions)}, {colors, sizeof(colors)}}, vds_size);
 
-    GLuint offset = 0;
-    vbo.declare_position_format(offset, 2, GL_FLOAT, 0);
+    //VBO vbo;
+    //GLuint vds_size = 5 * sizeof(GLfloat); // this vds has 2 attributes (position 2f + color 3f = 5f)
+    //vbo.initialize({{positions, sizeof(positions)}, {colors, sizeof(colors)}}, vds_size);
 
-    offset = sizeof(positions);
-    vbo.declare_color_format(offset, 3, GL_FLOAT, 0);
+    //GLuint offset = 0;
+    //vbo.declare_position_format(offset, 2, GL_FLOAT, 0);
 
-    vbo.render(GL_TRIANGLES);
+    //offset = sizeof(positions);
+    //vbo.declare_color_format(offset, 3, GL_FLOAT, 0);
+
+    //vbo.render(GL_TRIANGLES);
+
+    // With VBO - Eg. Format: |P|C|...|P|C|
+
+    GLfloat data[] =
+    {
+      -1.0F, +1.0F,
+      1.0F, 0.0F, 0.0F,
+
+      -0.5F, +1.0F,
+      1.0F, 0.0F, 0.0F,
+
+      -1.0F, -1.0F,
+      1.0F, 0.0F, 0.0F,
+
+      -1.0F, -1.0F,
+      0.0F, 1.0F, 0.0F,
+
+      -0.5F, +1.0F,
+      0.0F, 1.0F, 0.0F,
+
+      +0.5F, -1.0F,
+      0.0F, 1.0F, 0.0F,
+
+      +0.5F, -1.0F,
+      0.0F, 0.0F, 1.0F,
+
+      -0.5F, +1.0F,
+      0.0F, 0.0F, 1.0F,
+
+      +1.0F, +1.0F,
+      0.0F, 0.0F, 1.0F,
+
+      +1.0F, +1.0F,
+      1.0F, 0.0F, 1.0F,
+
+      +0.5F, -1.0F,
+      1.0F, 0.0F, 1.0F,
+
+      +1.0F, -1.0F,
+      1.0F, 0.0F, 1.0F,
+    };
+
+    //VBO vbo;
+
+    //GLuint vds_size = (3 + 2) * sizeof(GLfloat);
+    //vbo.initialize(&data, sizeof(data), vds_size);
+
+    //GLuint offset = 0;
+    //vbo.declare_position_format(0, 2, GL_FLOAT, vds_size);
+
+    //offset = 2*sizeof(GLfloat);
+    //vbo.declare_color_format(offset, 3, GL_FLOAT, vds_size);
+
+    //vbo.render(GL_TRIANGLES);
 
     // Without VBO
 
-    // glBegin(GL_TRIANGLES);
-    // {
-    //   glColor3f(1.0F, 0.0F, 0.0F);
-    //   glVertex2f(-1.0F, +1.0F);
-    //   glVertex2f(-0.5F, +1.0F);
-    //   glVertex2f(-1.0F, -1.0F);
-    // 
-    //   glColor3f(0.0F, 1.0F, 0.0F);
-    //   glVertex2f(-1.0F, -1.0F);
-    //   glVertex2f(-0.5F, +1.0F);
-    //   glVertex2f(+0.5F, -1.0F);
-    // 
-    //   glColor3f(0.0F, 0.0F, 1.0F);
-    //   glVertex2f(+0.5F, -1.0F);
-    //   glVertex2f(-0.5F, +1.0F);
-    //   glVertex2f(+1.0F, +1.0F);
-    // 
-    //   glColor3f(1.0F, 0.0F, 1.0F);
-    //   glVertex2f(+1.0F, +1.0F);
-    //   glVertex2f(+0.5F, -1.0F);
-    //   glVertex2f(+1.0F, -1.0F);
-    // }
-    // glEnd();
+    glBegin(GL_TRIANGLES);
+    {
+      glColor3f(1.0F, 0.0F, 0.0F);
+      glVertex2f(-1.0F, +1.0F);
+      glVertex2f(-0.5F, +1.0F);
+      glVertex2f(-1.0F, -1.0F);
+
+      glColor3f(0.0F, 1.0F, 0.0F);
+      glVertex2f(-1.0F, -1.0F);
+      glVertex2f(-0.5F, +1.0F);
+      glVertex2f(+0.5F, -1.0F);
+
+      glColor3f(0.0F, 0.0F, 1.0F);
+      glVertex2f(+0.5F, -1.0F);
+      glVertex2f(-0.5F, +1.0F);
+      glVertex2f(+1.0F, +1.0F);
+
+      glColor3f(1.0F, 0.0F, 1.0F);
+      glVertex2f(+1.0F, +1.0F);
+      glVertex2f(+0.5F, -1.0F);
+      glVertex2f(+1.0F, -1.0F);
+    }
+    glEnd();
   }
 };
